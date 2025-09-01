8e887c8d7ffe45e7bbf72bbee27d06d7a6ac62c8
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-01 14:13:18 +0545

    Create Separate Route folder and organize these routes

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// const express = require('express');
 import express from 'express';
-import { searchController, usernameController } from './controller.js';
+import router from './route.js';
 
 const app = express()
 
 const PORT =3000
 
-//DEFINE A SIMPLE ROUTE
-
 app.get('/', (req, res) => {
     res.send('Hello Express')
 })
 
-app.get('/user/:username', usernameController)
-
-// /search?keyword=express
-app.get('/search', searchController)
+app.use('/user',router)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
