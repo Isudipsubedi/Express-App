3fdaed0ed768fad5ac1d0c8e32c848b9835f7971
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-01 13:23:05 +0545

    Organize codes in separate files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const express = require('express');
 import express from 'express';
+import { searchController, usernameController } from './controller.js';
 
 const app = express()
 
@@ -11,16 +12,10 @@ app.get('/', (req, res) => {
     res.send('Hello Express')
 })
 
-app.get('/user/:username',(req, res)=>{
-    const username=req.params.username;
-    res.send(`Welcome ${username}`)
-})
+app.get('/user/:username', usernameController)
 
 // /search?keyword=express
-app.get('/search', (req, res)=>{
-    const keyword = req.query.keyword;
-    res.send(`Searching for ${keyword}`)
-})
+app.get('/search', searchController)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
