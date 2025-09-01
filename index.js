cd7d8720a3016402477130a1adce8216d3e8f707
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-01 12:59:50 +0545

    dynamic routing

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.get('/', (req, res) => {
     res.send('Hello Express')
 })
 
+app.get('/user/:username',(req, res)=>{
+    const username=req.params.username;
+    res.send(`Welcome ${username}`)
+})
+
+// /search?keyword=express
+app.get('/search', (req, res)=>{
+    const keyword = req.query.keyword;
+    res.send(`Searching for ${keyword}`)
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 } )
