fa372307fc70d40b6b7c5cdaca14192f74557f45
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-01 15:24:30 +0545

    HTTP METHODS GET, PUT, POST, DELETE Implemented

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ app.get('/', (req, res) => {
 
 app.use('/user',router)
 
+app.use(express.json())
+
+app.post('/users', (req, res)=>{
+    const {name, email} = req.body
+    res.json({
+        message:`User ${name} with email ${email} created successfully`
+    })
+})
+
+app.put('/users/:id', (req, res)=> {
+    const userId = req.params.id
+    const {name, email} = req.body
+    res.json({
+        message: `User ${userId} updated to ${name}, ${email}`
+    })
+})
+
+app.delete('/users/:id', (req, res)=> {
+    const userId = req.params.id
+    res.json({
+        message: `User with ID ${userId} deleted successfully`
+    })
+
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 } )
