e4670dffffc0dfdd479d4e1938237f81c8d81577
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-02 17:14:36 +0545

    Order of middleware Execution

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const app = express()
 
 const PORT =3000
 
-// '/welcome' for single route middleware
-app.use('/welcome', (req, res , next)=>{
-    console.log('A new req recieved at'+Date.now())
+app.use((req, res, next)=>{
+    console.log('Start')
+    res.on('finish',()=>{
+        console.log('end')    
+    })
     next()
 })
 
 app.get('/', (req, res) => {
+    console.log('middle')
     res.send('Hello Express')
 })
 
-app.get('/welcome', (req, res) => {
-    res.send('Welcome to express app')
-})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
