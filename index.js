fa5fa4bc24c48fb794552e46fa3b6552646b2825
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-02 17:42:00 +0545

    templating using ejs view

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const app = express()
 
 const PORT =3000
 
+
+//install package : npm i ejs 
+//ejs: embedded javascript
+
+//SET EJS as the view engine
+
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+   const userName = 'John Doe'
+   // index : view filename
+   res.render('index', {userName})
 })
 
 
-app.get('/error', ()=>{
-    throw new Error('This is test error')
-})
-
-app.use((err, req, res, next)=>{
-    console.error(err.message)
-    res.send('Internal server error')
-})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
