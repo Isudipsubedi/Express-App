fa07e3665ba3b2c4b6617d43b0f601ed27467c48
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-02 17:29:03 +0545

    error handling using middleware

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const app = express()
 
 const PORT =3000
 
-app.use((req, res, next)=>{
-    console.log('Start')
-    res.on('finish',()=>{
-        console.log('end')    
-    })
-    next()
-})
-
 app.get('/', (req, res) => {
-    console.log('middle')
     res.send('Hello Express')
 })
 
+
+app.get('/error', ()=>{
+    throw new Error('This is test error')
+})
+
+app.use((err, req, res, next)=>{
+    console.error(err.message)
+    res.send('Internal server error')
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
