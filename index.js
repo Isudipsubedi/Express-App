49060c78b8f4afdfd2904fdba8c2f93dabb6a6fd
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-02 17:08:47 +0545

    Middleware : application level , route level

--- index.js
+++ index.js
@@ -5,25 +5,19 @@ const app = express()
 
 const PORT =3000
 
+// '/welcome' for single route middleware
+app.use('/welcome', (req, res , next)=>{
+    console.log('A new req recieved at'+Date.now())
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('Hello Express')
 })
 
-app.get('/things/:name/:id', (req, res) => {
-   const { name, id } = req.params
-   if (!/^\d{5}$/.test(id)) {
-      return res.status(400).json({ error: 'ID must be exactly 5 digits' });
-   }
-   res.json({
-      id, name
-   })
+app.get('/welcome', (req, res) => {
+    res.send('Welcome to express app')
 })
-
-// catch all invalid routes
-app.get('/*any', (req, res)=>{
-   res.send('Sorry, this is an invalid route.')
-})
-
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
