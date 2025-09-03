0f59bb73a9bb8a8f333e2655e1f6bb1e32252dcc
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-03 15:38:38 +0545

    handling form data and save any file that we receive

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 import express from 'express';
+import multer from 'multer'
 
+//on sending post request from postman the file is saved to the uploads folder
+const storage = multer.diskStorage({
+    destination: 'uploads',
+    filename: (req, file, cb)=>{
+        cb(null, file.fieldname+'_'+Date.now()+file.originalname);
+    }
+})
 
 const app = express()
+const upload = multer({
+    storage:storage,
+    limits:{
+        fileSize:102400
+    }
 
+})
 const PORT =3000
 
-//PUG
-// app.use(express.static('public'))
-// app.use(express.static('images'))
 
-//to expose this folder on virtual path
-app.use('/public', express.static('public'))
-app.use('/images', express.static('images'))
-
+app.use(express.urlencoded({extended:true}))
+// app.use(upload.array())
+app.use(upload.single('image'))
 
 app.get('/', (req, res) => {
     res.send('Hello Express')
+})
+
+app.post('/form', (req, res)=>{
+    console.log(req.body)
+    console.log(req.file)
+    res.send('Form received')
 })
 
 
