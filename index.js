5a38321fdb034362658b334cf41a5ecbf64bbd27
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-05 16:21:08 +0545

    Session Management

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import session from 'express-session'
 
 const app = express()
 const PORT = 3000
 app.use(cookieParser())
 
+app.use(session({
+    secret: 'sample-secret',
+    resave: false,
+    saveUninitialized: false
+
+}))
+
 app.get('/', (req, res) => {
-    res.cookie('name','express-app', {maxAge: 360000})
     res.send('Hello Express')
 })
 
-app.get('/fetch',(req, res)=>{
-    console.log(req.cookies);
-    res.send('API Called')
+// To use SESSION we need external package ie. express session.
+//npm i express-session
+
+app.get('/visit', (req, res)=>{
+    if (req.session.page_views) {
+        req.session.page_views++;
+        res.send(`You visited this page ${req.session.page_views} times`)
+    } else {
+        req.session.page_views = 1;
+        res.send("Welcome to this page for the first time!")
+    }
 })
 
-app.get('/remove-cookie', (req, res)=>{
-    res.clearCookie('name')
-    res.send('cookie cleared')
+// How we can remove any session?
+
+app.get('/remove-visit', (req, res)=>{
+    req.session.destroy()
+    res.send('Session Removed')
 })
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
