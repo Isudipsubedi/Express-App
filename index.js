db7f08fe0f3c1c4ee21260b554868bb478680c47
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-08 14:14:14 +0545

    error handling

--- index.js
+++ index.js
@@ -5,64 +5,52 @@ const PORT = 3000
 
 app.use(express.json())
 
+//event listener : prevents app from crashing
+
+process.on("uncaughtException", (err)=>{
+    console.log(err)
+    process.exit(1)
+})
+
+// can add multiple event listener
+process.on('unhandledRejection',(reason, promise)=>{
+    console.log(reason);
+})
 
 app.get('/', (req, res) => {
     res.send('Hello Express')
 }) 
 
-//get all product
-app.get('/api/products', (req, res)=>{
-    const products = [
-        {id: 1, name: 'Laptop', price: 1000},
-        {id: 2, name: 'Desktop', price: 100},
-        {id: 3, name: 'Mobile', price: 300},
-    ]
-    res.status(200).json({products})
-})
-
-//get a single product
-app.get('/api/products/:id',(req, res)=>{
-        const products = [
-        {id: 1, name: 'Laptop', price: 1000},
-        {id: 2, name: 'Desktop', price: 100},
-        {id: 3, name: 'Mobile', price: 300},
-    ]
-
-    const product = products.find(p => p.id === Number(req.params.id))
-
-    if(!product){
-        return res.status(404).json({message:'Product not found'})
+// synchronous error
+app.get('/sync-error', (req, res , next) => {
+    try{
+        throw new Error('something went wrong')
+    } catch (error) {
+        next(error)
     }
-
-    res.status(200).json(product)
 })
 
-// Create a new product
-app.post('/api/products',(req, res)=>{
-    const newProduct = req.body
-    newProduct.id = Date.now()
-    res.status(201).json(newProduct)
+// asynchronous error
+app.get('/async-error', async(req, res, next)=>{
+    try {
+        await Promise.reject(new Error('Async Error Occured'))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+//To handle these error, we will craete middleware
+//Global error handling middleware
+
+app.use((err, req, res, next)=>{
+    console.error(err.message)
+    console.log(err.stack)
+    res.status(500).json({message:err.message})
 })
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
-
-
-
-/*
-PROJECT SCAFOLDING:
-
-npmjs.com: express-generator
-
-npm install -g express-generator
-
-express express-app-generated
-
---Folder created with name express-app-generated
-
-npm install
-
-npm start
-*/
