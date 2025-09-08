e14e84d1722df3d013e9f26d4868879b123330fe
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-09-08 12:00:13 +0545

    create restful apis

--- index.js
+++ index.js
@@ -1,61 +1,48 @@
 import express from 'express';
 
-// install packages: npm install jsonwebtoken, npm install bcryptjs
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 
-const users = []
 
 app.get('/', (req, res) => {
     res.send('Hello Express')
+}) 
+
+//get all product
+app.get('/api/products', (req, res)=>{
+    const products = [
+        {id: 1, name: 'Laptop', price: 1000},
+        {id: 2, name: 'Desktop', price: 100},
+        {id: 3, name: 'Mobile', price: 300},
+    ]
+    res.status(200).json({products})
 })
 
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body
-    const hashedPassword = await bcrypt.hash(password, 10)
-    users.push({
-        username,
-        password: hashedPassword
-    })
+//get a single product
+app.get('/api/products/:id',(req, res)=>{
+        const products = [
+        {id: 1, name: 'Laptop', price: 1000},
+        {id: 2, name: 'Desktop', price: 100},
+        {id: 3, name: 'Mobile', price: 300},
+    ]
 
-    res.send('user registered')
+    const product = products.find(p => p.id === Number(req.params.id))
 
-})
-
-
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body
-
-    const user = users.find(u => u.username === username)
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.send('Not authorized')
+    if(!product){
+        return res.status(404).json({message:'Product not found'})
     }
-    const token = jwt.sign({ username }, 'test#secret')
-    res.json({ token })
 
-
+    res.status(200).json(product)
 })
 
-app.get('/dashboard', (req, res) => {
-    try {
-        const token = req.header('Authorization')
-        const decodedToken = jwt.verify(token, 'test#secret')
-        if (decodedToken.username) {
-            res.send(`Welcome, ${decodedToken.username}`)
-        }
-        else {
-            res.send('access denied')
-        }
-    } catch (error) {
-        res.send('access denied')
-    }
+// Create a new product
+app.post('/api/products',(req, res)=>{
+    const newProduct = req.body
+    newProduct.id = Date.now()
+    res.status(201).json(newProduct)
 })
-
 
 
 app.listen(PORT, () => {
